3ea5e33bb38fcbfbd1368c6edf60aefb9b56da77
Stuart Green <stuartgreen@Stuarts-MacBook-Pro.local>
2022-02-26 22:39:23 +0000

    First draft at STS looks to work - need to double check results

--- main.py
+++ main.py
@@ -5,9 +5,12 @@ from math import ceil
 default_region = 'eu-west-1'
 
 
-def count_instances_and_nodes(region):  # Gather EC2 instances and EKS nodes
+def count_instances_and_nodes(region, session=None):  # Gather EC2 instances and EKS nodes
     #filters = []
-    client = boto3.client('ec2', region_name=region)
+    if session:
+        client = session.client('ec2', region_name=region)
+    else:
+        client = boto3.client('ec2', region_name=region)
     filters = [{
         "Name": "instance-state-name",
         "Values": ["running"]
@@ -33,8 +36,11 @@ def count_instances_and_nodes(region):  # Gather EC2 instances and EKS nodes
     return {"instances": billableInstanceCount, "nodes": billableNodeCount}
 
 
-def count_rds(region):  # Gather EC2 instances
-    client = boto3.client('rds', region_name=region)
+def count_rds(region, session=None):  # Gather EC2 instances
+    if session:
+        client = session.client('rds', region_name=region)
+    else:
+        client = boto3.client('rds', region_name=region)
     response = client.describe_db_instances(MaxRecords=100)
     billableRDSCount = 0
     # Declare non-billable instance types
@@ -45,8 +51,11 @@ def count_rds(region):  # Gather EC2 instances
     return billableRDSCount
 
 
-def count_lambdas(region):
-    client = boto3.client('lambda', region_name=region)
+def count_lambdas(region, session=None):
+    if session:
+        client = session.client('lambda', region_name=region)
+    else:    
+        client = boto3.client('lambda', region_name=region)
     response = client.list_functions()
 
 
@@ -66,7 +75,7 @@ def get_regions(region):
     return regions
 
 
-def all_regions_check():
+def all_regions_check(session=None):
     regions = get_regions(default_region)
     billableEC2 = 0
     billableLamb = 0
@@ -76,14 +85,47 @@ def all_regions_check():
 
     for reg in regions:
         print(f"[INFO] Checking region {reg}")
-        ec2_and_nodes = count_instances_and_nodes(reg)
+        ec2_and_nodes = count_instances_and_nodes(reg, session)
         billableEC2 += ec2_and_nodes['instances']
         billableNode += ec2_and_nodes['nodes']
-        billableLamb += count_lambdas(reg)
-        billableRDS += count_rds(reg)
+        billableLamb += count_lambdas(reg, session)
+        billableRDS += count_rds(reg, session)
 
     
     return {"totalEC2": billableEC2, "totalLambda": ceil(billableLamb / lambdaFactor), "totalK8sNodes": billableNode, "totalRDS": billableRDS}
+
+def org_mode():
+    # Assume role, get token, list accounts, assume to each account
+    # run commands
+    
+    orgClient = boto3.client('organizations')
+    resp = orgClient.list_accounts()
+    accountNumbers = list(map(lambda a: a['Id'], resp['Accounts']))
+    stsClient = boto3.client('sts')
+    currentAccount = stsClient.get_caller_identity()['Account']
+    
+    for account in accountNumbers:
+        # Don't assume into account if it's the current account for user
+
+        # Assume into each account
+        if account != currentAccount:
+            org_account_arn = 'arn:aws:iam::' + account + ':role/OrganizationAccountAccessRole'
+        
+        
+            stsResp = stsClient.assume_role(
+                RoleArn=org_account_arn,
+                RoleSessionName='CloudGuard_AssetCounter',
+                DurationSeconds=1800
+                )
+            stsSes = boto3.Session(
+                aws_access_key_id=stsResp['Credentials']['AccessKeyId'],
+                aws_secret_access_key=stsResp['Credentials']['SecretAccessKey'],
+                aws_session_token=stsResp['Credentials']['SessionToken']
+            )
+            # Pass the session to region handler, then iterate entity types
+            print(all_regions_check(stsSes))
+        else:
+            print(all_regions_check())
 
 
 def print_results(results):
@@ -95,5 +137,7 @@ def print_results(results):
     print(f"[INFO] Total billable EKS nodes: {results['totalK8sNodes']}")
 
 
-allRegionsCount = all_regions_check()
-print_results(allRegionsCount)
+#allRegionsCount = all_regions_check()
+#print_results(allRegionsCount)
+
+org_mode()
